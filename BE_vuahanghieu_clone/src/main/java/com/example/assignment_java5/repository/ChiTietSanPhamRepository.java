a7b3692aa6921dbaefad73f0af567c4a367bcae1
hdmmanh <hdmmanh26@gmail.com>
2024-07-25 11:13:59 +0700

    push 25/7/2024

--- BE_vuahanghieu_clone/src/main/java/com/example/assignment_java5/repository/ChiTietSanPhamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.assignment_java5.repository;
-
-import com.example.assignment_java5.model.ChiTietSanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChiTietSanPhamRepository extends JpaRepository<ChiTietSanPham, Integer> {
-}
--- BE_vuahanghieu_clone/src/main/java/com/example/assignment_java5/repository/DanhMucRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.assignment_java5.repository;
-
-import com.example.assignment_java5.model.DanhMuc;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DanhMucRepository extends JpaRepository<DanhMuc, Integer> {
-}
